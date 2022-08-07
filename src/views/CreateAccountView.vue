ba140f2b51a2347744e98aaa43dcc7d839041274
chemazu <chukwuemekachemazu@gmail.com>
2022-08-07 21:44:28 +0100

    first commit

--- src/views/CreateAccountView.vue
+++ src/views/CreateAccountView.vue
@@ -1,6 +1,32 @@
 <template>
   <div class="create-account">
-    <div class="create-account-card">Pp</div>
+    <div class="create-account-card">
+      <div className="heading">
+        <h2>Create an Account</h2>
+        <p>
+          Already have an account?
+          <router-link to="/about">Go to About</router-link>
+        </p>
+      </div>
+      <div className="form">
+        <form @submit.prevent="onSubmit" method="POST">
+          <div class="form-group">
+            <label for="name">Name</label>
+            <input
+              type="text"
+              class="form-control"
+              id="name"
+              v-model="name"
+              placeholder="Enter name"
+            />
+          </div>
+        </form>
+      </div>
+
+      <!-- <span class="error" v-if="errors.name">
+              {{ errors.name[0] }}
+            </span> -->
+    </div>
   </div>
 </template>
 
