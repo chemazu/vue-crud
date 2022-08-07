6a22e972804c480f4c53114aa79b8f6bb1d71ae9
chemazu <chukwuemekachemazu@gmail.com>
2022-08-07 21:29:02 +0100

    added router,sass

---
+++ src/views/CreateAccountView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="create-account">
+    <div class="create-account-card">Pp</div>
+  </div>
+</template>
+
+<script>
+import "../assets/styles/createaccount.scss";
+export default {
+  name: "CreateAccount",
+  data() {
+    return {
+      message: "CreateAccount",
+    };
+  },
+};
+</script>
+
+<style></style>
