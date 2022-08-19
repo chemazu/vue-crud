bb29c225309453ac7e02c141fbf358e42b63df3b
chemazu <chukwuemekachemazu@gmail.com>
2022-08-19 13:03:09 +0100

    login view created

--- src/router/index.ts
+++ src/router/index.ts
@@ -16,15 +16,7 @@ const routes: Array<RouteRecordRaw> = [
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
   },
-  {
-    path: "/create",
-    name: "create",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/CreateAccountView.vue"),
-  },
+
   {
     path: "/register",
     name: "register",
@@ -33,6 +25,12 @@ const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/RegisterView.vue"),
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/LoginView.vue"),
   },
 ];
 
---
+++ src/views/LoginView.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="login">
+    <div class="login-card">
+      <div class="heading">
+        <h2>Login</h2>
+        <p>If you have no account <Link to="/register">Sign up</Link></p>
+      </div>
+      <form @submit.prevent="handleSubmit">
+        <div className="form-item">
+          <p>Email Address</p>
+          <input type="email" v-model="email" required />
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.login {
+  background-color: #fafafa;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100%;
+}
+</style>
